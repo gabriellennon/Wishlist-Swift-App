a79cb2138ea079d887dcd6b862d83c26927cd6e6
gabriel.lennon <gabriel.lennon@ifood.com.br>
2025-02-12 10:44:50 -0300

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Gabriel Lennon on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
